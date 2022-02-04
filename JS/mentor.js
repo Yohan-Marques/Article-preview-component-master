7a8e1d20fdbe94c31cc78fee78e85b1ac03e6edc
Breno Yohan <breno@zettaware.com.br>
2022-02-03 23:39:40 -0300

    firts commit

---
+++ JS/mentor.js
@@ -0,0 +1,38 @@
+
+
+var share = document.querySelector('.icon');
+var icone = document.querySelector('#icone');
+
+share.addEventListener('click', () => {
+    icone.classList.toggle('active');
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*{
+    if(document.getElementById("icone").style.display="flex")
+{
+    document.getElementById("icone").style.display="none"
+
+}
+else if((document.getElementById("icone").style.display="none"))
+{
+    document.getElementById("icone").style.display="flex"
+}
+}*/
